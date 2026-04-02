efb2a139452df9b89df615fd6f61da7062bc5a6b
MinjoonCho <jaydencho0324@gmail.com>
2026-04-02 18:00:40 +0900

    java-3rd-assignment

--- src/Payment.java
+++ src/Payment.java
@@ -1,7 +1,30 @@
 public class Payment {
-    // 결제 방식 (카드, 카카오페이 등)
-    public String type;
-    
-    // 결제 금액
-    public int amount;
+    // Payment 객체 개선
+    private String type;
+    private int amount;
+
+    // 로직 위치 변경
+    public Payment(String type, int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("금액이 올바르지 않습니다.");
+        }
+        this.type = type;
+        this.amount = amount;
+    }
+
+    // 메서드 추가
+    public boolean isMatchedType(String targetType) {
+        return this.type.equals(targetType);
+    }
+
+    // 메서드 추가
+    public void executePayment() {
+        if (isMatchedType("card")) {
+            System.out.println("카드 결제: " + this.amount);
+        } else if (isMatchedType("kakao")) {
+            System.out.println("카카오페이 결제: " + this.amount);
+        } else {
+            System.out.println("지원하지 않는 결제 방식");
+        }
+    }
 }
